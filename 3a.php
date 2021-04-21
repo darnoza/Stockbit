330ae31cf5be068647022caf9ebcb40d8d65d9b1
darnoza <darnoza@gmail.com>
2021-04-21 08:03:36 +0700

    Update 3a.php

--- 3a.php
+++ 3a.php
@@ -1,1 +1,12 @@
-3a
+func findFirstStringInBrackets(s string) string {
+    i := strings.IndexByte(s, '(')
+    if i < 0 {
+        return ""
+    }
+    i++
+    j := strings.IndexByte(s[i:], ')')
+    if j < 0 {
+        return ""
+    }
+    return s[i : i+j]
+}
